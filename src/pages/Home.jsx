c81785157ee92a10a18c4e64d130cfc4d0df8bf5
Dhruval Patel <dhruvalpatel273@gmail.com>
2025-09-22 00:59:37 +0530

    text chnaged

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,27 +11,38 @@ const Home = () => {
     <>
       {!isLoaded && (
         <Loader
-          images={['/videos/video.mp4']} // video preloaded by loader
+          images={["/videos/video.mp4"]} // video preloaded by loader
           onLoadComplete={() => setIsLoaded(true)}
         />
       )}
 
       {isLoaded && (
         <div className="relative">
+          {/* Background video */}
           <div className="h-screen w-screen fixed bg-black z-0">
             <Video onReady={() => setIsLoaded(true)} />
           </div>
 
-          <div className="h-screen w-screen relative pb-5 flex flex-col justify-between z-10">
+          {/* Content Layer */}
+          <div className="h-screen w-screen relative flex flex-col justify-between z-10">
+            {/* Hero Title */}
             <HomeHerotxt />
-            <div className="pl-[50%] relative z-[15]">
-              <p className="text-white lg:text-gray-300 text-[10px] lg:text-[15px] manrope1 mr-10 transition-opacity duration-1000 ease-in-out">
-                "Turning ideas into bold, unforgettable designs. At Drimin Design,
-                we craft brands, websites, and digital experiences that captivate,
-                inspire, and make your vision a reality."
-              </p>
+
+            {/* Side-aligned paragraph */}
+            <div className="flex justify-end pr-8 relative z-[15]">
+              <div className="max-w-[320px]">
+                <p className="text-white lg:text-gray-300 text-[12px] lg:text-[12px] leading-relaxed manrope1 opacity-90">
+                  "Turning ideas into bold, unforgettable designs. At Drimin
+                  Design, we craft brands, websites, and digital experiences
+                  that captivate, inspire, and make your vision a reality."
+                </p>
+              </div>
             </div>
-            <HomeBottomtxt />
+
+            {/* Bottom centered navigation buttons */}
+            <div className="flex justify-center pb-6">
+              <HomeBottomtxt />
+            </div>
           </div>
         </div>
       )}
